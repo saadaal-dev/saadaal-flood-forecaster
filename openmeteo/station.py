889d092e88fe9df46003b0ec2e2a9d9701000530
Allister SANCHEZ <allister.sanchez@gmail.com>
2024-11-13 14:10:16 +0100

    feat: added weather data for districts

--- openmeteo/station.py
+++ openmeteo/station.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Station:
     def __init__(self, id: int, name: str, latitude: float, longitude: float):
         self.id = id
@@ -7,3 +10,20 @@ class Station:
 
     def __str__(self):
         return f"Station [{self.id}]: {self.name}, Latitude: {self.latitude}, Longitude: {self.longitude}"
+
+
+def get_stations(csv_path: str) -> list[Station]:
+    with open(csv_path, mode="r", newline="") as location_file:
+        reader = csv.reader(location_file)
+        stations: list[Station] = []
+        next(reader, None)  # skip the headers
+        for row in reader:
+            station = Station(
+                id=int(row[0]),
+                name=row[1],
+                latitude=float(row[3]),
+                longitude=float(row[4]),
+            )
+            print(station)
+            stations.append(station)
+        return stations
