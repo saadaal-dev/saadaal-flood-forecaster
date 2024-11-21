d997e08ce3294b4a60337246a4fe0f70a0158141
Adina M <acm1107@gmail.com>
2024-11-21 08:36:28 +0100

    Basic linter and secrets detection; apply code fixes; update readme

--- data-extractor/latest_river.py
+++ data-extractor/latest_river.py
@@ -1,15 +1,17 @@
-from bs4 import BeautifulSoup
+import os
+
 import pandas as pd
 import psycopg2
 import requests
-import os
+from bs4 import BeautifulSoup
 
-POSTGRES_PASSWORD = os.environ['POSTGRES_PASSWORD']
+POSTGRES_PASSWORD = os.environ["POSTGRES_PASSWORD"]
 
 conn_string = f"postgresql://postgres:{POSTGRES_PASSWORD}@68.183.13.232:5432/postgres"
 
 conn = psycopg2.connect(conn_string)
 cursor = conn.cursor()
+
 
 def fetch_station_id(station_name):
     cursor.execute(f"SELECT id FROM station WHERE name = '{station_name}'")
@@ -19,11 +21,12 @@ def fetch_station_id(station_name):
     else:
         return None
 
+
 def insert_data(df):
     for index, row in df.iterrows():
-        station = row['station']
-        reading = row['reading']
-        reading_date = row['reading_date']
+        station = row["station"]
+        reading = row["reading"]
+        reading_date = row["reading_date"]
 
         station_id = fetch_station_id(station)
         if station_id is None:
@@ -40,22 +43,21 @@ def insert_data(df):
 
 
 def scrape_data():
-    url = 'https://frrims.faoswalim.org/rivers/levels'
+    url = "https://frrims.faoswalim.org/rivers/levels"
     response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    table = soup.find('table', id='maps-data-grid')
+    soup = BeautifulSoup(response.content, "html.parser")
+    table = soup.find("table", id="maps-data-grid")
     df = pd.read_html(str(table))[0]
     df = df.head(7)
     new_df = pd.DataFrame()
-    new_df['station'] = df['Station'].astype(str)
-    new_df['reading'] = pd.to_numeric(df['Observed River Level (m)'], errors='coerce')
-    new_df['reading_date'] = pd.to_datetime(df['Date'],format="%d-%m-%Y").dt.date
+    new_df["station"] = df["Station"].astype(str)
+    new_df["reading"] = pd.to_numeric(df["Observed River Level (m)"], errors="coerce")
+    new_df["reading_date"] = pd.to_datetime(df["Date"], format="%d-%m-%Y").dt.date
 
-    return new_df    
+    return new_df
     return df
 
 
 data = scrape_data()
 print(data)
 insert_data(data)
-
--- data-extractor/new_river.py
+++ data-extractor/new_river.py
@@ -1,17 +1,17 @@
-import requests
-from tqdm import tqdm
-import pandas as pd
-from sqlalchemy import create_engine,text
 import os
 
-POSTGRES_PASSWORD = os.environ['POSTGRES_PASSWORD']
+import pandas as pd
+import requests
+from sqlalchemy import create_engine, text
+from tqdm import tqdm
 
-url = 'http://frrims.faoswalim.org/rivers/graph'
+POSTGRES_PASSWORD = os.environ["POSTGRES_PASSWORD"]
+
+url = "http://frrims.faoswalim.org/rivers/graph"
+
 
 def get_data(station_id):
-    form_data = {
-        "station_id": station_id
-    }
+    form_data = {"station_id": station_id}
 
     try:
         print(url)
@@ -22,54 +22,65 @@ def get_data(station_id):
         if response.text:  # Check if the response is not empty
             json_response = response.json()
             print(json_response)
-            gauge_reading_list = json_response['gaugeReadingList']
+            gauge_reading_list = json_response["gaugeReadingList"]
 
             return gauge_reading_list
         else:
-            print('Empty response')
+            print("Empty response")
             return None
 
     except requests.exceptions.HTTPError as http_err:
-        print(f'HTTP error occurred: {http_err}')  # Python 3.6
+        print(f"HTTP error occurred: {http_err}")  # Python 3.6
     except requests.exceptions.RequestException as err:
-        print(f'Error occurred: {err}')  # Python 3.6
+        print(f"Error occurred: {err}")  # Python 3.6
     except ValueError:
-        print('Response is not a valid JSON')
+        print("Response is not a valid JSON")
     except requests.exceptions.Timeout:
         print("The request timed out")
         return None
 
-def insert_data(data,station_id):
+
+def insert_data(data, station_id):
     df = pd.DataFrame(data)
-    df['station_id'] = station_id
+    df["station_id"] = station_id
     # Convert readingValue to numeric and rename to 'reading'
-    df['reading'] = pd.to_numeric(df['readingValue'])
+    df["reading"] = pd.to_numeric(df["readingValue"])
 
     # Convert dateOfReadingStr to datetime and rename to 'reading_date'
-    df['reading_date'] = pd.to_datetime(df['dateOfReadingStr'],format="%d-%m-%Y").dt.date
+    df["reading_date"] = pd.to_datetime(
+        df["dateOfReadingStr"], format="%d-%m-%Y"
+    ).dt.date
     # Create a SQLAlchemy engine
-    engine = create_engine(f"postgresql://postgres:{POSTGRES_PASSWORD}@68.183.13.232:5432/postgres")
+    engine = create_engine(
+        f"postgresql://postgres:{POSTGRES_PASSWORD}@68.183.13.232:5432/postgres"
+    )
 
     # Write data to PostgreSQL
-    df[['station_id','reading', 'reading_date']].to_sql('temp_table', engine, if_exists='replace', index=False)
+    df[["station_id", "reading", "reading_date"]].to_sql(
+        "temp_table", engine, if_exists="replace", index=False
+    )
     with engine.begin() as connection:
-        query = text("""
+        query = text(
+            """
             INSERT INTO station_river_data (station_id, reading, reading_date)
             SELECT station_id, reading, reading_date
             FROM temp_table
             ON CONFLICT (station_id, reading_date) DO NOTHING
-        """)
+        """
+        )
         connection.execute(query)
+
+
 stations = [
-    {'id': 1, 'name': 'Luuq'},
-    {'id': 6, 'name': 'Jowhar'},
-    {'id': 2, 'name': 'Dollow'},
-    {'id': 5, 'name': 'Bulo Burti'},
-    {'id': 3, 'name': 'Bualle'},
-    {'id': 4, 'name': 'Belet Weyne'},
-    {'id': 17, 'name': 'Bardheere'},
+    {"id": 1, "name": "Luuq"},
+    {"id": 6, "name": "Jowhar"},
+    {"id": 2, "name": "Dollow"},
+    {"id": 5, "name": "Bulo Burti"},
+    {"id": 3, "name": "Bualle"},
+    {"id": 4, "name": "Belet Weyne"},
+    {"id": 17, "name": "Bardheere"},
 ]
 
 for station in tqdm(stations, desc="Processing stations", unit="station"):
-    data = get_data(station['id'])
-    insert_data(data,station['id'])
+    data = get_data(station["id"])
+    insert_data(data, station["id"])
