13d0c1dc0d5bd5d87810f712c467592093645bb5
falvarezetcheverry <florencia.alvarezetcheverry@amadeus.com>
2024-12-04 17:30:23 +0100

    Add module to fetch and load data

---
+++ src/flood_forecaster/data_ingestion/load.py
@@ -0,0 +1,12 @@
+
+# Methods to load the data from the database
+
+# Load the history weather data from the database and returns it as a pandas dataframe
+def load_history_weather_db(settings, date_begin = "", date_end = ""):
+    pass
+
+def load_forecast_weather_db(settings, date_begin = "", date_end = ""):
+    pass
+
+def load_river_level_db(settings, date_begin = "", date_end = ""):
+    pass
