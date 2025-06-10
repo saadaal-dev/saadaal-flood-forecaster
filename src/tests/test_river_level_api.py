78d65d5601dcc6dcd3b30a744db2a229e99e7dd8
FlorEtcheverry <florencia.a.etcheverry@gmail.com>
2025-06-10 16:38:50 +0200

    Add river level api test

---
+++ src/tests/test_river_level_api.py
@@ -0,0 +1,10 @@
+from src.flood_forecaster.data_ingestion.swalim.river_level_api import fetch_latest_river_data
+from src.flood_forecaster.utils.configuration import Config
+
+
+def test_fetch_latest_river_data():
+    # Mock configuration
+    config = Config("tests/mock_config.ini")
+    historical_river_levels = fetch_latest_river_data(config)
+    # print(historical_river_levels)
+    # assert len(historical_river_levels) > 0, "Expected to fetch historical river levels"
