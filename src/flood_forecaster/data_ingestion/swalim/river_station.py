b5d0b46258e3a9f9a6d02bbdf8c49ed95592f8cc
Maxime PETER <maxime.peter@amadeus.com>
2025-06-11 15:39:51 +0200

    Creation of the risk_assesment module

---
+++ src/flood_forecaster/data_ingestion/swalim/river_station.py
@@ -0,0 +1,29 @@
+import csv
+from typing import List
+
+from src.flood_forecaster.data_ingestion.openmeteo.station import Station
+
+class RiverStation(Station):
+    def __init__(self, id: int, name: str, latitude: float, longitude: float, moderate_threshold: float, high_threshold: float, full_threshold: float = 0.0):
+        super().__init__(id, name, latitude, longitude)
+        self.moderate_threshold = moderate_threshold
+        self.high_threshold = high_threshold
+        self.full_threshold = full_threshold
+
+def get_river_stations(csv_path: str) -> List[RiverStation]:
+    with open(csv_path, mode="r", newline="") as location_file:
+        reader = csv.reader(location_file)
+        stations: List[RiverStation] = []
+        next(reader, None)  # skip the headers
+        for row in reader:
+            station = RiverStation(
+                id=int(row[0]),
+                name=row[1],
+                latitude=float(row[3]),
+                longitude=float(row[4]),
+                moderate_threshold=float(row[7]),
+                high_threshold=float(row[8]),
+                full_threshold=float(row[9])
+            )
+            stations.append(station)
+        return stations
---
+++ src/flood_forecaster/risk_assesment/risk_assesment.py
@@ -0,0 +1,70 @@
+from sqlalchemy import update
+from src.flood_forecaster.utils.configuration import Config
+from src.flood_forecaster.data_ingestion.swalim.river_station import get_river_stations, RiverStation
+from src.flood_forecaster.utils.database_helper import DatabaseConnection
+from src.flood_forecaster.data_model.river_level import PredictedRiverLevel
+
+
+def create_update_statement(river_station: RiverStation, risk_level: str) -> update:
+    """
+    Create an SQL update statement to set the risk level for a river station.
+    
+    :param river_station: The river station for which the risk level is being set.
+    :param risk_level: The risk level to be set (e.g., 'low', 'moderate', 'high', 'full').
+    :return: An SQLAlchemy update statement.
+    """
+    match risk_level:
+        case "low":
+            return update(PredictedRiverLevel)\
+                        .values(risk_level=risk_level)\
+                        .where(PredictedRiverLevel.station_number == river_station.id)\
+                        .where(PredictedRiverLevel.risk_level == None)\
+                        .where(PredictedRiverLevel.level_m < river_station.moderate_threshold)
+        case "moderate":
+            return update(PredictedRiverLevel)\
+                        .values(risk_level=risk_level)\
+                        .where(PredictedRiverLevel.station_number == river_station.id)\
+                        .where(PredictedRiverLevel.risk_level == None)\
+                        .where(PredictedRiverLevel.level_m >= river_station.moderate_threshold)\
+                        .where(PredictedRiverLevel.level_m < river_station.high_threshold)
+        case "high":
+            return update(PredictedRiverLevel)\
+                        .values(risk_level=risk_level)\
+                        .where(PredictedRiverLevel.station_number == river_station.id)\
+                        .where(PredictedRiverLevel.risk_level == None)\
+                        .where(PredictedRiverLevel.level_m >= river_station.high_threshold)\
+                        .where(PredictedRiverLevel.level_m < river_station.full_threshold)
+        case "full":
+            return update(PredictedRiverLevel)\
+                        .values(risk_level=risk_level)\
+                        .where(PredictedRiverLevel.station_number == river_station.id)\
+                        .where(PredictedRiverLevel.risk_level == None)\
+                        .where(PredictedRiverLevel.level_m >= river_station.full_threshold)
+        
+
+def execute_sql_update(river_station: RiverStation, risk_level: str, database_connection: DatabaseConnection):
+    """
+    Execute an SQL update statement to set the risk level for a river station.
+    
+    :param river_station: The river station for which the risk level is being set.
+    :param risk_level: The risk level to be set (e.g., 'low', 'moderate', 'high', 'full').
+    :param database_connection: Database connection object.
+    """
+    update_stmt = create_update_statement(river_station, risk_level)
+    with database_connection.engine.connect() as conn:
+        result = conn.execute(update_stmt)
+        conn.commit()
+        print(f"Row(s) updated for station {river_station.id} with risk level {risk_level}: {result.rowcount}")
+
+
+config = Config(config_file_path="config/config.ini")
+data_config = config.load_csv_config()
+database_connection = DatabaseConnection(config)
+river_stations = get_river_stations(f"{data_config["data_path"]}{data_config["river_stations_metadata_file_name"]}")
+thresholds = ["low", "moderate", "high", "full"]
+
+for river_station in river_stations:
+    print(f"Processing station: {river_station.name}")
+    print(f"Moderate threshold: {river_station.moderate_threshold}, High threshold: {river_station.high_threshold}, Full threshold: {river_station.full_threshold}")
+    for threshold in thresholds:
+        execute_sql_update(river_station, threshold, database_connection)
