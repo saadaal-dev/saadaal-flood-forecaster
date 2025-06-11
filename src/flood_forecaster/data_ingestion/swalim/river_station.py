ef27ada237750464878ca925668256f3fb9dc1ac
maximepeter <maxime.peter97@gmail.com>
2025-06-11 16:13:09 +0200

    Update the ml module to use the RiverStation class

--- src/flood_forecaster/data_ingestion/swalim/river_station.py
+++ src/flood_forecaster/data_ingestion/swalim/river_station.py
@@ -4,11 +4,13 @@ from typing import List
 from src.flood_forecaster.data_ingestion.openmeteo.station import Station
 
 class RiverStation(Station):
-    def __init__(self, id: int, name: str, latitude: float, longitude: float, moderate_threshold: float, high_threshold: float, full_threshold: float = 0.0):
+    def __init__(self, id: int, name: str, latitude: float, longitude: float, region: str, district: str, moderate_threshold: float, high_threshold: float, full_threshold: float = 0.0):
         super().__init__(id, name, latitude, longitude)
         self.moderate_threshold = moderate_threshold
         self.high_threshold = high_threshold
         self.full_threshold = full_threshold
+        self.region = region
+        self.district = district
 
 def get_river_stations(csv_path: str) -> List[RiverStation]:
     with open(csv_path, mode="r", newline="") as location_file:
@@ -21,6 +23,8 @@ def get_river_stations(csv_path: str) -> List[RiverStation]:
                 name=row[1],
                 latitude=float(row[3]),
                 longitude=float(row[4]),
+                region=row[5],
+                district=row[6],
                 moderate_threshold=float(row[7]),
                 high_threshold=float(row[8]),
                 full_threshold=float(row[9])
